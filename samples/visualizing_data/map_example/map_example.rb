9da6250f665098f9266b6b762abade89ef5e07f7
phren0logy <andrew.nanton@lobesandrobes.com>
2009-01-16 14:56:40 -0500

    "troubleshooting lerp_color"

--- samples/visualizing_data/map_example/map_example.rb
+++ samples/visualizing_data/map_example/map_example.rb
@@ -19,40 +19,39 @@ class MapExample < Processing::App
     puts @data.size
     @data_min = @data.transpose[1].min.to_f
     @data_max = @data.transpose[1].max.to_f
-
   end
+    
+    def draw
+       background 0
+       image @img, 0, 0
 
-  def draw
-    background 0
-    image @img, 0, 0
+      CSV.open("./samples/visualizing_data/map_example/data/locations.tsv", 'r', col_sep = "\t") do |row|
+         x_position = row[1].to_i
+         y_position = row[2].to_i
+         @locations << [x_position, y_position]
+       end
 
-   CSV.open("./samples/visualizing_data/map_example/data/locations.tsv", 'r', col_sep = "\t") do |row|
-      x_position = row[1].to_i
-      y_position = row[2].to_i
-      @locations << [x_position, y_position]
+       # each_with_index brings in @locations, an array of other two-member
+       # arrays.  The two members are the x and y coordinates, accessed here
+       # as xy[0] for x and xy[1] for y. Index numbers them to access the
+       # the equivalent line number in @data, but it is not accessed by
+       # state name as in the original example.
+       @locations.each_with_index do |xy, index|
+         value = @data[index][1].to_f
+         puts value
+         size = map(value, @data_min, @data_max, 2, 40)
+         # map function is from processing, NOT from ruby.  It takes a
+         # value, then two ranges of numbers.  The first range is the
+         # original data min/max, and the second is target min/max.
+         # It then scales the value to its proporional position in
+         # the new range.  Ruby's map function is totally different.
+         percent = norm(value, @data_min, @data_max)
+         puts percent
+         between = lerp_color("FF4422".hex, "4422CC".hex, percent) # 4th parameter is HSB
+         fill between
+         ellipse xy[0], xy[1], size, size
+       end
     end
-
-    # each_with_index brings in @locations, an array of other two-member
-    # arrays.  The two members are the x and y coordinates, accessed here
-    # as xy[0] for x and xy[1] for y. Index numbers them to access the
-    # the equivalent line number in @data, but it is not accessed by
-    # state name as in the original example.
-    @locations.each_with_index do |xy, index|
-      value = @data[index][1].to_f
-      size = map(value, @data_min, @data_max, 2, 40)
-      # map function is from processing, NOT from ruby.  It takes a
-      # value, then two ranges of numbers.  The first range is the
-      # original data min/max, and the second is target min/max.
-      # It then scales the value to its proporional position in
-      # the new range.  Ruby's map function is totally different.
-      percent = norm(value, @data_min, @data_max)
-      between = lerp_color('ff4422', '4422cc', percent)
-      fill 255, 0, 0
-      ellipse xy[0], xy[1], size, size
-    end
-
-
-  end
 
 end
 
